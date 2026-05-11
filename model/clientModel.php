061a91eb421e4716b7050971f2daeeb3278edaf6
crybaby050 <benthiam750@gmail.com>
2026-05-11 22:13:18 +0000

    test de suppression de donné

---
+++ model/clientModel.php
@@ -0,0 +1,24 @@
+<?php
+require_once __DIR__ . "/../config/config.php";
+
+function deleteClient($id){
+    $pdo = getPDO();
+
+    $sql = "DELETE FROM client WHERE id_client = :id";
+
+    $stmt = $pdo->prepare($sql);
+
+    $stmt->execute([
+        'id' => $id
+    ]);
+
+    return $stmt->rowCount();
+}
+
+$result = deleteClient(1);
+
+if($result > 0){
+    echo "Client supprimé";
+}else{
+    echo "Client introuvable";
+}
