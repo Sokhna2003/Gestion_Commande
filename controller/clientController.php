c9646b84fa692d060aa7667a9cf048da5dcb1181
crybaby05 <benthiam750@gmail.com>
2026-05-12 00:04:21 +0000

    omevoer

--- controller/clientController.php
+++ controller/clientController.php
@@ -1,42 +1,20 @@
 <?php
 require_once __DIR__."/../model/clientModel.php";
+echo "je suis dasn le model";
 function newClient()
 {
-
+echo "je suis dasn la fonction d'ajout";
     if (isset($_POST['add-client'])) {
         $nom = $_POST['nom'];
         $prenom = $_POST['prenom'];
         $email = $_POST['email'];
         $telephone = $_POST['telephone'];
         $adresse = $_POST['adresse'];
-        // $error = [];
-        // Validation
-        // if (empty($nom)) $error['nom'] = 'Champ obligatoire';
-        // if (empty($pre)) $error['pre'] = 'Champ obligatoire';
-        // if (empty($cls)) $error['cls'] = 'Champ obligatoire';
-        // if (empty($tel)) $error['tel'] = 'Champ obligatoire';
-        // if (empty($ads)) $error['ads'] = 'Champ obligatoire';
-        // if (empty($mail)) {
-        //     $error['mail'] = 'Champ obligatoire';
-        // } else if (!is_email($mail)) {
-        //     $error['mail'] = 'Mail invalide';
-        // }
-        // Vérification des doublons
-        // $user_mail = verifUniqueUniversel($mail, 'email', 'etudiant');
-        // if ($user_mail) {
-        //     $error['mail'] = 'Utilisateur déjà enregistré';
-        // }
-        // $user_tel = verifUniqueUniversel($tel, 'telephone', 'etudiant');
-        // if ($user_tel) {
-        //     $error['tel'] = 'Numéro déjà occupé';
-        // }
-        // Transformation de la classe
-        // $id_classe = getIdClasseByLibelle($cls);
-        // if (!$id_classe) {
-        //     $error['cls'] = 'Classe invalide';
-        // }
-        // if (empty($error)) {
-             ajoutClient($nom, $prenom, $telephone, $email, $adresse);
+echo "avant ajout";
+
+            ajoutClient($nom, $prenom, $telephone, $email, $adresse);
+echo "aprés ajout";
+
             // return [$error, $success];
             header('Location: ajout.php');
         // }
--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php 
-define("WEBROOT","http://localhost:8002/");
-define("ROOT", (substr($_SERVER['DOCUMENT_ROOT'] ,0, -6)));
+//define("WEBROOT","http://localhost:8002/");
+//define("ROOT", (substr($_SERVER['DOCUMENT_ROOT'] ,0, -6)));
 
      // require_once __DIR__."/views/client/lister.php";
-     require_once __DIR__."/controller/clientController.php";
+     //echo "je te vous";
+     //require_once __DIR__."/controller/clientController.php";
+     require_once __DIR__ . "/views/client/ajout.php";
+     //newClient();
+     echo "je te j jk fj jf jf ";
 // require_once __DIR__."/views/client/ajout.php";
 
 ?>
