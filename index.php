f4329494b0c88b20ceb63786bf42484f3956219d
sokhna <dianesokhna2003@gmail.com>
2026-05-12 09:05:07 +0000

    mise en page lister produit

--- index.php
+++ index.php
@@ -3,8 +3,11 @@ define("WEBROOT","http://localhost:8002/");
 define("ROOT", (substr($_SERVER['DOCUMENT_ROOT'] ,0, -6)));
 
      // require_once __DIR__."/views/client/lister.php";
-     require_once __DIR__."/controller/clientController.php";
-     newClient();
+
+     require_once __DIR__."/views/produit/lister.php";
+
+     // require_once __DIR__."/controller/clientController.php";
+     // newClient();
 
 // require_once __DIR__."/views/client/ajout.php";
 
