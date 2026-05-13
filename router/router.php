6a52715e2c0d6091f4cc78113ebe8b90f4d6972a
crybaby05 <benthiam750@gmail.com>
2026-05-13 08:52:59 +0000

    initialisation du fichier rooter.php

--- router/router.php
+++ router/router.php
@@ -1,1 +1,16 @@
 <?php
+$controllers = [
+    "client" => "client",
+    "produit" => "produit",
+    "commande" => "commande"
+];
+
+$controller = $_REQUEST['controller'] ?? "client";
+
+if (array_key_exists($controller,$controllers)){
+    $path = ROOT."controller/".$controllers[$controller]."Controller.php";
+}else{
+    echo "controller introuvable";
+}
+
+require_once($path);
