ffbe06f46f21c86fc972cdb25a787a9b14ba2504
crybaby05 <benthiam750@gmail.com>
2026-05-13 11:46:45 +0000

    knknwd

--- controller/clientController.php
+++ controller/clientController.php
@@ -1,12 +1,18 @@
 <?php
 require_once __DIR__."/../model/clientModel.php";
 
+// Tableau des actions disponibles
 $actions = [
-    "lister" => "listerClient"
+    "lister" => "listeClient",
+    "new" => "newClient",
+    "supprimer" => "supprimerClient",
+    "modifier" => "modifierClient"
 ];
 
-$action = $_REQUEST['action'] ?? 'listerClient';
+// Récupération de l'action (par défaut "lister")
+$action = $_REQUEST['action'] ?? "lister";
 
+// Exécution de l'action si elle existe
 if (array_key_exists($action, $actions)) {
     $fonction = $actions[$action];
     if (function_exists($fonction)) {
@@ -18,52 +24,76 @@ if (array_key_exists($action, $actions)) {
     echo "Erreur : action '$action' non trouvée";
 }
 
+// Actions
+function listeClient(){
+    $clients = listerClient();
+    // Utiliser ROOT au lieu de __DIR__
+    $vuePath = ROOT . "views/client/lister.php";
+    
+    if (file_exists($vuePath)) {
+        require_once $vuePath;
+    } else {
+        echo "Vue introuvable : " . $vuePath;
+    }
+}
+
 function newClient()
 {
     if (isset($_POST['add-client'])) {
-
         $nom = $_POST['nom'];
         $prenom = $_POST['prenom'];
         $email = $_POST['email'];
         $telephone = $_POST['telephone'];
         $adresse = $_POST['adresse'];
-            ajoutClient($nom, $prenom, $telephone, $email, $adresse);
-            header("Location:".WEBROOT."?page=lister");
-            exit();
+        
+        ajoutClient($nom, $prenom, $telephone, $email, $adresse);
+        
+        header("Location: " . WEBROOT . "?controller=client&action=lister");
+        exit();
     }
-
-    require_once __DIR__ . '/../views/client/ajout.php';
+    
+    $vuePath = ROOT . "views/client/ajout.php";
+    if (file_exists($vuePath)) {
+        require_once $vuePath;
+    } else {
+        echo "Vue introuvable : " . $vuePath;
+    }
 }
 
-function supprimerCLient(){
+function supprimerClient(){
     if(isset($_GET['delete'])){
         $id = intval($_GET['delete']);
         deleteClient($id);
-        header("Location:".WEBROOT."?page=lister");
+        header("Location: " . WEBROOT . "?controller=client&action=lister");
+        exit();
     }
 }
+
 function modifierClient()
 {
     if(isset($_GET['id'])){
-
         $id = (int) $_GET['id'];
-
         $client = getClientById($id);
-
+        
         if(isset($_POST['update-client'])){
-
             $nom = $_POST['nom'];
             $prenom = $_POST['prenom'];
             $telephone = $_POST['telephone'];
             $email = $_POST['email'];
             $adresse = $_POST['adresse'];
-
+            
             updateClient($id, $nom, $prenom, $telephone, $email, $adresse);
-
-            header("Location:".WEBROOT."?page=lister");
+            
+            header("Location: " . WEBROOT . "?controller=client&action=lister");
             exit();
         }
-
-        require_once __DIR__."/../views/client/ajout.php";
+        
+        $vuePath = ROOT . "views/client/ajout.php";
+        if (file_exists($vuePath)) {
+            require_once $vuePath;
+        } else {
+            echo "Vue introuvable : " . $vuePath;
+        }
     }
 }
+?>
--- public/index.php
+++ public/index.php
@@ -6,29 +6,7 @@
 
      //define("ROOT", substr($_SERVER["DOCUMENT_ROOT"], 0, -6));
 
-        require_once ROOT . "/controller/produitController.php";
-        require_once ROOT . "/controller/clientController.php";
+    require_once ROOT . "router/router.php";
 
-     
-$page = $_GET['page'] ?? 'lister';
-
-if ($page == "delete") {
-    supprimerCLient();
-}
-elseif ($page == "lister") {
-    $clients = listerClient();
-    require_once ROOT . "/views/client/lister.php";
-
-}
-elseif ($page == "ajout") {
-    newClient();
-}elseif($page == "update"){
-     modifierClient();
-}elseif($page == "listerp"){
-     $produits = listerProduit();
-    require_once ROOT . "/views/produit/listerp.php";
-}
-     
-// require_once __DIR__."/views/client/ajout.php";
 
 ?>
