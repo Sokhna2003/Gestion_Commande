63e207edac5f99f783367aeb0c6db0b68cd7d37a
sokhna <dianesokhna2003@gmail.com>
2026-05-19 22:13:28 +0000

    mise en page lister commande

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
      define("ROOT",substr($_SERVER["DOCUMENT_ROOT"],0,-6));
 
      //define("ROOT", substr($_SERVER["DOCUMENT_ROOT"], 0, -6));
-
+    require_once ROOT."views/fixe/header.php";
     require_once ROOT . "router/router.php";
 
 
