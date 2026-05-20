342f75e9a61536890c947b0e83aba30bdb35d109
crybaby05 <benthiam750@gmail.com>
2026-05-20 19:22:58 +0000

    fonction manquante dans le helper

--- config/helper.php
+++ config/helper.php
@@ -17,3 +17,13 @@ function loadView(string $view, array $datas=[]){
 function path(string $controller, string $action): string{
     return WEBROOT."?controller=$controller&action=$action";
 }
+
+function redirectTo(string $controller, string $action):void{
+    header('Location:'.WEBROOT."?controller=$controller&action=$action");
+    exit();
+
+}
+function countTable(string $table){
+    $sql="SELECT COUNT(*) as total FROM $table";
+   return executeSelect($sql,[],true)["total"];
+}
