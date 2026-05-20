9413c14a33ab390169be26ea038004c87c7ffd73
ngd <ndeyegalassdiagne30@gmail.com>
2026-05-20 23:33:35 +0000

    modififacation des fichiers pour le CRUD client

--- config/validator.php
+++ config/validator.php
@@ -17,8 +17,48 @@ function isString($value){
 
 function isMail($value){
     return filter_var($value, FILTER_VALIDATE_EMAIL);
-}   
+}
 
 function validate(array $errors):bool{
     return count($errors)==0;
+}
+
+function emailExiste(string $email, int $excludeId = 0): bool {
+    $pdo = getPDO();
+    $sql = "SELECT COUNT(*) as total FROM client WHERE email = :email AND id_client != :id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(['email' => $email, 'id' => $excludeId]);
+    return $stmt->fetch()['total'] > 0;
+}
+
+function telephoneExiste(string $telephone, int $excludeId = 0): bool {
+    $pdo = getPDO();
+    $sql = "SELECT COUNT(*) as total FROM client WHERE telephone = :telephone AND id_client != :id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(['telephone' => $telephone, 'id' => $excludeId]);
+    return $stmt->fetch()['total'] > 0;
+}
+
+function validDataClient(array $data, int $excludeId = 0):array{
+     $errors = [];
+        if(empty($data["nom"])){
+            $errors["nomVide"] ="Veuillez remplir le nom";
+        }
+        if(empty($data["prenom"])){
+            $errors["prenomVide"] ="Veuillez remplir le prenom";
+        }
+        if(empty($data["telephone"])){
+            $errors["telephoneVide"] ="Veuillez remplir le telephone";
+        } elseif(telephoneExiste($data["telephone"], $excludeId)){
+            $errors["telephoneDuplique"] ="Ce numéro de telephone est déjà utilisé";
+        }
+        if(empty($data["email"])){
+            $errors["email"] ="Veuillez remplir l'email";
+        } elseif(emailExiste($data["email"], $excludeId)){
+            $errors["emailDuplique"] ="Cet email est déjà utilisé";
+        }
+        if(empty($data["adresse"])){
+            $errors["adresse"] ="Veuillez remplir l'adresse";
+        }
+        return $errors;
 }
--- controller/clientController.php
+++ controller/clientController.php
@@ -1,99 +1,67 @@
 <?php
-require_once __DIR__."/../model/clientModel.php";
+require_once ROOT."/model/clientModel.php";
 
-// Tableau des actions disponibles
-$actions = [
-    "lister" => "listeClient",
-    "new" => "newClient",
-    "supprimer" => "supprimerClient",
-    "modifier" => "modifierClient"
-];
-
-// Récupération de l'action (par défaut "lister")
-$action = $_REQUEST['action'] ?? "lister";
-
-// Exécution de l'action si elle existe
-if (array_key_exists($action, $actions)) {
-    $fonction = $actions[$action];
-    if (function_exists($fonction)) {
-        $fonction();
-    } else {
-        echo "Erreur : fonction '$fonction' non trouvée";
-    }
-} else {
-    echo "Erreur : action '$action' non trouvée";
-}
-
-// Actions
-function listeClient(){
+$liste = function(){
     $clients = listerClient();
-    // Utiliser ROOT au lieu de __DIR__
-    $vuePath = ROOT . "views/client/lister.php";
-    
-    if (file_exists($vuePath)) {
-        require_once $vuePath;
-    } else {
-        echo "Vue introuvable : " . $vuePath;
-    }
-}
+    $total_client = countClients();
+    loadView("client/lister", ["clients"=>$clients, "total_client"=>$total_client]);
+};
 
-function newClient()
-{
-    if (isset($_POST['add-client'])) {
-        $nom = $_POST['nom'];
-        $prenom = $_POST['prenom'];
-        $email = $_POST['email'];
-        $telephone = $_POST['telephone'];
-        $adresse = $_POST['adresse'];
-        
-        ajoutClient($nom, $prenom, $telephone, $email, $adresse);
-        
-        header("Location: " . WEBROOT . "?controller=client&action=lister");
-        exit();
+$ajout = function(){
+    if(isset($_POST['add-client'])){
+        $errors = validDataClient($_POST);
+        if(validate($errors)){
+            ajoutClient($_POST['nom'], $_POST['prenom'], $_POST['telephone'], $_POST['email'], $_POST['adresse']);
+            redirectTo("client", "liste");
+        }
+        loadView("client/ajout", ["errors" => $errors, "old" => $_POST]);
+        return;
     }
-    
-    $vuePath = ROOT . "views/client/ajout.php";
-    if (file_exists($vuePath)) {
-        require_once $vuePath;
-    } else {
-        echo "Vue introuvable : " . $vuePath;
-    }
-}
+    loadView("client/ajout");
+};
 
-function supprimerClient(){
-    if(isset($_GET['delete'])){
-        $id = intval($_GET['delete']);
-        deleteClient($id);
-        header("Location: " . WEBROOT . "?controller=client&action=lister");
-        exit();
-    }
-}
+$detail = function(){
+    echo "je detail un client";
+};
 
-function modifierClient()
-{
+$modifier = function(){
     if(isset($_GET['id'])){
         $id = (int) $_GET['id'];
         $client = getClientById($id);
-        
         if(isset($_POST['update-client'])){
-            $nom = $_POST['nom'];
-            $prenom = $_POST['prenom'];
-            $telephone = $_POST['telephone'];
-            $email = $_POST['email'];
-            $adresse = $_POST['adresse'];
-            
-            updateClient($id, $nom, $prenom, $telephone, $email, $adresse);
-            
-            header("Location: " . WEBROOT . "?controller=client&action=lister");
-            exit();
+            $errors = validDataClient($_POST, $id);
+            if(validate($errors)){
+                updateClient($id, $_POST['nom'], $_POST['prenom'], $_POST['telephone'], $_POST['email'], $_POST['adresse']);
+                redirectTo("client", "liste");
+            }
+            $client = $_POST;
+            loadView("client/ajout", ["client" => $client, "errors" => $errors]);
+            return;
         }
-        
-        $vuePath = ROOT . "views/client/ajout.php";
-        if (file_exists($vuePath)) {
-            require_once $vuePath;
-        } else {
-            echo "Vue introuvable : " . $vuePath;
-        }
+        loadView("client/ajout", ["client"=>$client]);
     }
+};
+
+$supprimer = function(){
+    if(isset($_GET['delete'])){
+        deleteClient((int)$_GET['delete']);
+        redirectTo("client", "liste");
+    }
+};
+
+$actions = [
+    "liste"     => $liste,
+    "ajout"     => $ajout,
+    "detail"    => $detail,
+    "modifier"  => $modifier,
+    "supprimer" => $supprimer
+];
+
+$action = $_REQUEST["action"] ?? "liste";
+
+if(array_key_exists($action, $actions)){
+    $actions[$action]();
+} else {
+    echo "page introuvable c client";
+    exit();
 }
-?>
