e07f7a36cf060b3ead007da8479e349d0f36a0af
crybaby05 <benthiam750@gmail.com>
2026-05-20 21:19:22 +0000

    quelque fonction

--- config/helper.php
+++ config/helper.php
@@ -9,9 +9,13 @@ function dd($test)
     die("Yallah bakhna");
 }
 
-function loadView(string $view, array $datas=[]){
+
+function loadView(string $view,array $datas=[],string $layout="base") {
+    ob_start();
     extract($datas);
-    require_once (ROOT."view/".$view.".php");
+    require_once(ROOT."views/".$view.".php");
+    $content=ob_get_clean();
+    require_once ROOT."/views/layout/$layout.layout.php";
 }
 
 function path(string $controller, string $action): string{
