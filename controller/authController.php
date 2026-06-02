7ad32f46130096a0daea22a6ebcc30fde310fd45
Sokhna2003 <dianesokhna2003@gmail.com>
2026-06-02 17:20:28 +0000

    realisation de la fonctionnalité connexion et deconnexion et l'ajout de la page dashboard.php et auth.layout.php (#40)

--- controller/authController.php
+++ controller/authController.php
@@ -1,1 +1,49 @@
-dashboard
+<?php
+require_once ROOT."/model/authModel.php";
+
+$logout = function(){
+    session_unset();    // Vide les données de session (ex: $_SESSION['user'])
+    session_destroy();  // Détruit la session sur le serveur
+    redirectTo("auth","login");
+
+};
+
+$login = function(){
+    if(isConnected()){
+        redirectTo("dashboard","index");
+    }
+    
+   $errors=[];
+    if (isset($_POST["connect"])) {
+        isEmpty("emailVide",$_POST["email"],$errors,"Veuillez renseigner l'email");
+        isEmpty("passwordVide",$_POST["password"],$errors,"Veuillez renseigner le password");
+        
+        if (validate($errors)) {
+            $user=login($_POST["email"]);
+            // dd($user);
+            if ($user && $_POST["password"] == $user["mdp"]) {
+                $_SESSION["user"]=$user;
+                redirectTo("dashboard","index");
+            }else{
+                $errors["global"]="email ou mot de passe incorrect";
+            }
+        }
+    }
+loadView("auth/login",["errors"=>$errors],"auth");
+};
+
+
+$actions=[
+    "login"=>$login,
+    "logout"=>$logout, 
+];
+ $action=$_REQUEST["action"]??"login";
+ 
+ if (array_key_exists($action, $actions)) {
+         $actions[$action]();
+     }
+     else{
+         echo "page introuvable c client";
+         exit();
+}
+         
